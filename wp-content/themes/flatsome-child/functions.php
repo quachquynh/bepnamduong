26fc45485d23cbfe4fd307ba4d547201795efdc1
quachquynh <81851320+quachquynh@users.noreply.github.com>
2023-02-12 15:43:16 +0700

    Update

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,2 +1,27 @@
 <?php
-// Add custom Theme Functions here
+
+add_filter( 'rwmb_meta_boxes', 'product_metabox' );
+function product_metabox( $meta_boxes ) {
+    $meta_boxes[] = [
+        'title'   => esc_html__( 'Menu', 'online-generator' ),
+        'post_types' => 'product',
+        'context' => 'normal',
+        'fields'  => [
+            [
+                'id'      => 'product-code',
+                'name'    => 'Mã sản phẩm',
+                'type'    => 'text'
+            ],
+        ],
+    ];
+    
+    return $meta_boxes;
+}
+
+/*
+ * WC Hooks
+ */
+function product_code() {
+
+}
+add_filter();
